8526fd94b29a5856d0d87ae8a87d1ca702669813
PedroSalvino <pedro_luiz7199@outlook.com>
2022-06-04 09:42:11 -0300

    1 file modified, 1 added

--- src/app/terminal.service.ts
+++ src/app/terminal.service.ts
@@ -1,8 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Terminal, TerminalApiResponse } from '../interfaces';
+import { GlobalService } from './global.service';
 
 @Injectable()
 export class TerminalService {
+  terminais: Terminal[] = [];
+  terminalPicked: Terminal = {} as Terminal;
+  query: '';
 
-  constructor() { }
+  constructor(private http: HttpClient, public global: GlobalService) {}
 
+  getTerminal(id: number) {
+    this.http
+      .get<Terminal>(`${this.global.api}/terminal/${id}`)
+      .subscribe((data) => {
+        if (data) this.terminalPicked = data;
+      });
+  }
+
+  getTerminais() {
+    this.http
+      .get<TerminalApiResponse>(`${this.global.api}/terminal/page`)
+      .subscribe((data) => {
+        if (data) this.terminais = data.content;
+      });
+  }
 }
