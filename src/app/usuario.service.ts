ec38609379ef05abf28547d8bed7eabf35451313
PedroSalvino <pedro_luiz7199@outlook.com>
2022-06-04 17:43:08 -0300

    1 file added

---
+++ src/app/usuario.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  PostUsuario,
+  PutUsuario,
+  Usuario,
+  UsuarioApiResponse,
+} from '../interfaces';
+import { GlobalService } from './global.service';
+
+@Injectable()
+export class UsuarioService {
+  usuarios: Usuario[] = [];
+  usuarioPicked: Usuario = {} as Usuario;
+
+  constructor(private http: HttpClient, public global: GlobalService) {}
+
+  getUsuario(id: number) {
+    this.http
+      .get<Usuario>(`${this.global.api}/usuario/${id}`)
+      .subscribe((data) => {
+        if (data) this.usuarioPicked = data;
+      });
+  }
+
+  getUsuarios() {
+    this.http
+      .get<UsuarioApiResponse>(`${this.global.api}/usuario/page`)
+      .subscribe((data) => {
+        if (data) this.usuarios = data.content;
+      });
+  }
+
+  postUsuario(body: PostUsuario) {
+    this.http
+      .post(`${this.global.api}/usuario`, body)
+      .subscribe((data) => console.log(data));
+
+    console.log(body);
+  }
+
+  putUsuario(body: PutUsuario) {
+    this.http
+      .put(`${this.global.api}/usuario`, body)
+      .subscribe((data) => console.log(data));
+
+    console.log(body);
+  }
+
+  deleteUsuario(id: number) {
+    this.http.delete<any>(`${this.global.api}/usuario/${id}`);
+  }
+}
